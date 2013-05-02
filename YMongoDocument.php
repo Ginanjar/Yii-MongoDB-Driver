834967c7989e84aad2286a8cce70f5bef498f39c
Maksim Naumov <fromyukki@gmail.com>
2013-05-02 20:11:32 +0400

    Basic update & insert functions

--- YMongoDocument.php
+++ YMongoDocument.php
@@ -18,11 +18,25 @@ abstract class YMongoDocument extends CModel
     public static $db;
 
     /**
+     * Default name of the Mongo primary key
+     *
+     * @var MongoId
+     */
+    public $_id;
+
+    /**
      * class name => model
      *
      * @var array
      */
     private static $_models = array();
+
+    /**
+     * Static cache for attribute names
+     *
+     * @var array
+     */
+    private static $_attributes = array();
 
     /**
      * whether this instance is new or not
@@ -82,6 +96,182 @@ abstract class YMongoDocument extends CModel
     }
 
     /**
+     * Saves the current record.
+     *
+     * @param bool $runValidation
+     * @param array $attributes
+     * @return bool
+     */
+    public function save($runValidation = true, $attributes = null)
+    {
+        if (!$runValidation || $this->validate($attributes)) {
+            return $this->getIsNewRecord() ? $this->insert($attributes) : $this->update($attributes);
+        }
+        return false;
+    }
+
+    /**
+     * Inserts a row into the table based on this active record attributes.
+     *
+     * @param array $attributes
+     * @return bool
+     * @throws YMongoException
+     */
+    public function insert($attributes = null)
+    {
+        if (!$this->getIsNewRecord()) {
+            throw new YMongoException(Yii::t('yii','The active record cannot be inserted to database because it is not new.'));
+        }
+
+        if ($this->beforeSave()) {
+            Yii::trace(get_class($this).'.insert()', 'ext.mongoDb.YMongoDocument');
+
+            // Create new MongoId if not set
+            $this->{$this->primaryKey()} = $this->getPrimaryKey();
+
+            // Insert new record
+            try {
+                $res = $this->getCollection()->insert(
+                    $this->getDocument(),
+                    $this->getConnection()->getDefaultWriteConcern()
+                );
+
+                /**
+                 * Returns an array containing the status of the insertion if the "w" option is set.
+                 * Otherwise, returns TRUE if the inserted array is not empty
+                 */
+                if (true === $res || (is_array($res) && !empty($res['ok']))) {
+                    $this->afterSave();
+                    $this->setIsNewRecord(false);
+                    $this->setScenario(self::SCENARIO_UPDATE);
+                    return true;
+                }
+            } catch (Exception $e) { }
+        }
+        return false;
+    }
+
+    /**
+     * Updates the row represented by this active record.
+     *
+     * @param array $attributes
+     * @return bool
+     * @throws YMongoException
+     */
+    public function update($attributes = null)
+    {
+        if ($this->getIsNewRecord()) {
+            throw new YMongoException(Yii::t('yii','The active record cannot be updated because it is new.'));
+        }
+
+        if ($this->beforeSave()) {
+            Yii::trace(get_class($this).'.update()', 'ext.mongoDb.YMongoDocument');
+
+            $pk = $this->primaryKey();
+
+            if (null === $this->{$pk}) {
+                throw new YMongoException(Yii::t('yii','The active record cannot be updated because it has no _id.'));
+            }
+
+            $result = false;
+
+            // Save whole document
+            if (null === $attributes) {
+                try {
+                    $res = $this->getCollection()->save(
+                        $this->getDocument(),
+                        $this->getConnection()->getDefaultWriteConcern()
+                    );
+
+                    /**
+                     * If w was set, returns an array containing the status of the save.
+                     * Otherwise, returns a boolean representing if the array was not empty.
+                     */
+                    if (true === $res || (is_array($res) && !empty($res['ok']))) {
+                        $result = true;
+                    }
+                } catch (Exception $e) { }
+            }
+            // Save only specify attributes
+            else {
+                // Prepare document
+                $document = $this->getDocument($attributes);
+
+                if (isset($document[$pk])) {
+                    unset($document[$pk]);
+                }
+
+                try {
+                    $res = $this->getCollection()->update(
+                        array($pk => $this->{$pk}), // criteria
+                        array('$set' => $document), // new object
+                        $this->getConnection()->getDefaultWriteConcern()
+                    );
+
+                    /**
+                     * Returns an array containing the status of the update if the "w" option is set.
+                     * Otherwise, returns TRUE.
+                     */
+                    if (true === $res || (is_array($res) && !empty($res['ok']))) {
+                        $result = true;
+                    }
+                } catch (Exception $e) { }
+            }
+
+            // Everything alright
+            if (true === $result) {
+                $this->afterSave();
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Get the names of the attributes of the class
+     *
+     * @return array
+     */
+    public function attributeNames()
+    {
+        $className = get_class($this);
+
+        if (!isset(self::$_attributes[$className])) {
+            /**
+             * Initialize an empty array with the names of the attributes.
+             * Static cache is still necessary, even with the finding that no attributes.
+             */
+            self::$_attributes[$className] = array();
+
+            // Class data
+            $class = new ReflectionClass($className);
+            $classProperties = $class->getProperties(ReflectionProperty::IS_PUBLIC);
+
+            foreach($classProperties as $property) {
+                if ($property->isStatic()) {
+                    continue;
+                }
+                self::$_attributes[$className][] = $property->getName();
+            }
+        }
+
+        return self::$_attributes[$className];
+    }
+
+    /**
      * Returns the static model of the specified AR class.
      *
      * EVERY derived AR class must override this method as follows,
@@ -97,14 +287,15 @@ abstract class YMongoDocument extends CModel
      */
     public static function model($className = __CLASS__)
     {
-        if (isset(self::$_models[$className])) {
-            return self::$_models[$className];
+        if (!isset(self::$_models[$className])) {
+            /** @var YMongoDocument $model */
+            $model = new $className(null);
+            $model->attachBehaviors($model->behaviors());
+
+            self::$_models[$className] = $model;
         }
 
-        /** @var YMongoDocument $model */
-        $model = self::$_models[$className] = new $className(null);
-        $model->attachBehaviors($model->behaviors());
-        return $model;
+        return self::$_models[$className];
     }
 
     /**
@@ -128,6 +319,20 @@ abstract class YMongoDocument extends CModel
     }
 
     /**
+     * Returns MongoId based on $value
+     *
+     * @param mixed $value
+     * @return MongoId
+     */
+    public function getPrimaryKey($value = null)
+    {
+        if (null === $value) {
+            $value = $this->{$this->primaryKey()};
+        }
+        return ($value instanceof MongoId) ? $value : new MongoId($value);
+    }
+
+    /**
      * Returns the database connection used by active record.
      *
      * @return YMongoClient
@@ -147,6 +352,16 @@ abstract class YMongoDocument extends CModel
         } else {
             throw new YMongoException(Yii::t('yii','YMongoDocument a "mongoDb" YMongoClient application component.'));
         }
+    }
+
+    /**
+     * Gets the collection for this model
+     *
+     * @return MongoCollection
+     */
+    public function getCollection()
+    {
+        return $this->getConnection()->getCollection($this->collectionName());
     }
 
     /**
@@ -277,5 +492,59 @@ abstract class YMongoDocument extends CModel
         if ($this->hasEventHandler('onAfterFind')) {
             $this->onAfterFind(new CEvent($this));
         }
+    }
+
+    /**
+     * Filters a provided document to take out mongo objects.
+     *
+     * @param mixed $document
+     * @return array
+     */
+    public function filterDocument($document)
+    {
+        if (is_array($document)) {
+            /** @var $value array|YMongoDocument */
+            foreach($document as $key => $value) {
+                // Recursive
+                if (is_array($value)) {
+                    $document[$key] = $this->filterDocument($value);
+                }
+                // Nested documents does are not allowed at this time
+                elseif($value instanceof YMongoDocument) {
+                    unset($document[$key]);
+                }
+            }
+        }
+        return $document;
+    }
+
+    /**
+     * Gets the raw document with mongo objects taken out
+     *
+     * @param array $attributes
+     * @return array
+     */
+    public function getDocument($attributes = null)
+    {
+        if (!is_array($attributes) || empty($attributes)) {
+            $attributes = $this->attributeNames();
+        }
+        $document = array();
+
+        foreach($attributes as $field) {
+            $document[$field] = $this->{$field};
+        }
+
+        return $this->filterDocument($document);
+    }
+
+    /**
+     * Gets the JSON encoded document
+     *
+     * @return string
+     */
+    public function getJSONDocument()
+    {
+        return CJSON::encode($this->getDocument());
     }
 }
